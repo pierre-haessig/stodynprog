c861aa257ba48dd99b8ce7c4992a7c058952d852
Pierre Haessig <pierre.haessig@crans.org>
2015-12-16 12:00:23 +0100

    add run_tests function

--- stodynprog/__init__.py
+++ stodynprog/__init__.py
@@ -13,3 +13,18 @@ from __future__ import (division, print_function, unicode_literals,
                         absolute_import)
 
 from .stodynprog import SysDescription, DPSolver
+
+
+def run_tests(verbose=False):
+    '''run the unit tests of stodynprog (using `nose` module)
+    
+    return True if test run succeeded, False otherwise
+    '''
+    import nose
+    import sys
+    print('Stodynprog unit tests:')
+    argv = [sys.argv[0]]
+    if verbose:
+        argv.append('--verbose')
+    success = nose.run(argv=argv)
+    return success
